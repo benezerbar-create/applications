0b069b306cc91cc6d0c3f973c191d5ce46e1e99a
bar <benezerbar@gmail.com>
2025-11-05 16:03:43 +0200

    test

--- app/src/main/java/com/katza/myapplicationbar/MainActivity.java
+++ app/src/main/java/com/katza/myapplicationbar/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "start", Toast.LENGTH_SHORT).show();
 
         initViews();
 
